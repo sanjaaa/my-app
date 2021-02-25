0b871dfdf463640b0608900565dab5cf21a00c5e
sanjana <sanj.s.mane@gmail.com>
2021-02-25 10:05:02 +0530

    init

--- src/index.js
+++ src/index.js
@@ -1,17 +1,54 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import { ReactReduxFirebaseProvider } from "react-redux-firebase";
+import { createFirestoreInstance } from "redux-firestore";
+import { rootReducer } from "./ducks/reducers";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB463E4D9ruWjVYKVqWmw6gGTlrHhx_dT8",
+  authDomain: "firedux-todo-d43d3.firebaseapp.com",
+  projectId: "firedux-todo-d43d3",
+  storageBucket: "firedux-todo-d43d3.appspot.com",
+  messagingSenderId: "644970774230",
+  appId: "1:644970774230:web:2b41a0180beaa0c0a3d377",
+  measurementId: "G-WEM3JN5TEK",
+};
+
+// react-redux-firebase config
+const rrfConfig = {
+  userProfile: "users",
+  useFirestoreForProfile: true, // Firestore for Profile instead of Realtime DB
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.firestore();
+
+const initialState = {};
+const store = createStore(rootReducer, initialState);
+
+const rrfProps = {
+  firebase,
+  config: rrfConfig,
+  dispatch: store.dispatch,
+  createFirestoreInstance, //since we are using Firestore
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <ReactReduxFirebaseProvider {...rrfProps}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ReactReduxFirebaseProvider>
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
